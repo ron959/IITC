093a82bc3f97ffc301c03911421c1ff1f05ffd16
ron959 <9592414@gmail.com>
2024-10-12 19:31:45 +0300

    my project

--- fronted mentor/project/main.js
+++ fronted mentor/project/main.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all buttons inside #numbers-container and all buttons on the page
+    
     const numButtons = document.querySelectorAll('#numbers-container button');
     const allBtns = document.querySelectorAll('button');
     const numSelected = document.getElementById('selected-number');    
 
-    // Variable to keep track of the previously selected button
     let previousButton = null;
 
-    // Add hover (mouseenter and mouseleave) event listeners for all buttons
+    // mouseenter and mouseleave event listeners for all buttons
     allBtns.forEach(button => {
         button.addEventListener('mouseenter', function() {
             if (button !== previousButton) { // Only change color if it's not the selected button
@@ -46,12 +45,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const thankContainer = document.getElementById('thank-container');
     const ratingContainer = document.getElementById('rating-container');
 
-    // Add event listener to the submit button
     submitBtn.addEventListener('click', () => {
         if (previousButton === null) {
             alert('You have to click a number to submit');
         } else {
-            // Show the thank-you container and hide the rating container
             thankContainer.classList.remove('hidden');
             ratingContainer.classList.add('hidden');
         }
